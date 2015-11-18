4ea11b58018fb8381dbf65e6d68d146fba8d8291
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-11-18 16:20:00 -0600

    some additions

--- repo_scraper/GitChecker.py
+++ repo_scraper/GitChecker.py
@@ -3,39 +3,47 @@ import re
 from repo_scraper.matchers import password_matcher
 
 def list_commits():
+    '''Returns a list with all commit hashes'''
     p = subprocess.Popen(['git', 'log', '--pretty=format:%H'],
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
     out, err = p.communicate()
     return out.replace('"', '').split('\n')
 
-#use diff instead of show
+
 def diff_for_commit(commit_hash):
+    '''Retrieves diff for a specific commit and parses it
+       to get file name and additions'''
     p = subprocess.Popen(['git', 'diff', commit_hash],
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE)
     out, err = p.communicate()
     #For some reason, this commands is returning with ""\n at the
     #beginning of the file
-    return out.replace('""\n', '')
+    diff = out.replace('""\n', '')
+    return parse_diff(diff)
 
-#http://stackoverflow.com/questions/2529441/how-to-read-the-output-from-git-diff
-def parse_commit_diff(diff):
+def parse_diff(diff):
+    '''Parse a diff output'''
     files = diff.split('diff --git ')[1:]
     files = [parse_file_diff(f) for f in files]
     return files
 
+#http://stackoverflow.com/questions/2529441/how-to-read-the-output-from-git-diff
 def parse_file_diff(diff):
+    '''Parse a diff oputput'''
     lines = diff.split('\n')
     filename = lines[0]
     content = filter(lambda x: x.startswith('+'), lines[1:])
     content = reduce(lambda x,y:x+'\n'+y, content) if len(content) else ''
     return {'filename': filename, 'content': content}
 
-for commit in commits[:10]:
-    files = parse_commit_diff(diff_for_commit(commit))
+commits = list_commits()
+for commit in commits[8:10]:
+    print 'Checking commit %s' % commit
+    files = diff_for_commit(commit)
     for f in files:
         has_password, matches = password_matcher(f['content'])
         if has_password:
-            print 'Commit %s \nHas passwords in file %s, \nmatches: %s' % (commit, f['filename'], matches)
+            print 'Passwords in file %s, \nmatches: %s\n\n' % (f['filename'], matches)
     print '-----------------------'
