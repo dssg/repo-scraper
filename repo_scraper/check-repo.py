6051ce7ab1709690db263cc027f68ab7a80b55fe
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-11-20 15:10:25 -0600

    adds check-repo script to bin

--- repo_scraper/check-repo.py
+++
@@ -1,7 +0,0 @@
-from repo_scraper.GitChecker import GitChecker 
-
-gc = GitChecker(["py", "ipynb", "json", "sql", "sh", "txt", "r", "md", "log"]).file_traverser()
-
-for result in gc:
-    if result.result_type=='ALERT':
-        print result
