6862ddc49ac82e850a0e1ef9f729dda1347edaeb
Eduardo Blancas Reyes <edu.blancas@gmail.com>
2015-11-24 14:50:08 -0600

    creates constants module

---
+++ repo_scraper/constants/extensions.py
@@ -0,0 +1,4 @@
+#Add extensions here (lowercase)
+DEFAULT_EXTENSIONS = ["py", "ipynb", "json", "sql", "sh", "txt", "r", "md", "log"]
+DEFAULT_EXTENSIONS_FORMAT = reduce(lambda x,y: x+', '+y, DEFAULT_EXTENSIONS)
+
--- repo_scraper/constants/git_diff.py
+++ repo_scraper/constants/git_diff.py
@@ -1,7 +1,3 @@
-#Add extensions here (lowercase)
-DEFAULT_EXTENSIONS = ["py", "ipynb", "json", "sql", "sh", "txt", "r", "md", "log"]
-DEFAULT_EXTENSIONS_FORMAT = reduce(lambda x,y: x+', '+y, DEFAULT_EXTENSIONS)
-
 #Constants in git.py (used in git diff commands)
 #Maximum number of lines when doing git diff hash_a hash_b
 #that won't trigger a BIG_FILE result, note that this constant
@@ -10,4 +6,3 @@ MAX_DIFF_LINES = 10000
 #Max number of characters in ADDITIONS that won't trigger a BIG_FILE result,
 #note that in this case, only new lines are taken into account
 MAX_DIFF_ADDITIONS_CHARACTERS = 1048576
-
